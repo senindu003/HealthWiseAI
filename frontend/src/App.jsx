b3c24c73f9b3529142cc1b26a3e60dc46b2a2156
senindu003 <senindudinapura@gmail.com>
2026-06-26 22:00:14 +0530

    health questionnaire page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+import React from "react";
+import HealthAssessmentWizard from "./components/HealthAssessmentWizard";
+
+const App = () => {
+  return (
+    <div>
+      <HealthAssessmentWizard />
+    </div>
+  );
+};
+
+export default App;
